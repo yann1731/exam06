a6cc718def9648a3cf3764e06ed66bef5dc2a566
yann_1731 <yan_1731@hotmail.com>
2023-12-23 11:27:33 -0500

    let's go

--- mini_serv.c
+++ mini_serv.c
@@ -1,12 +1,20 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "unistd.h"
-#include "string.h"
-#include "sys/select.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 int main(int argc, char *argv[]) {
 	if (argc == 2) {
+		int sock;
 		int port = atoi(argv[1]);
+		if (sock = socket(AF_INET, SOCK_STREAM, 0)) {
+			write(STDERR_FILENO, "Fatal error\n", strlen("Fatal error\n"));
+			exit(1);
+		}
+		
 
 	}
 	else {
