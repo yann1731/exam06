c067fd2afd65b06706aecae1d317db2a826eab13
yann <yan_1731@hotmail.com>
2023-12-31 13:54:25 -0500

    Trying to work this out. Got to figure out handling of client messages now

--- mini_serv.c
+++ mini_serv.c
@@ -36,12 +36,15 @@ char *getMessage(int fd) {
 
 int main(int argc, char *argv[]) {
 	if (argc == 2) {
+		printf("Server starting\n");
 		int server;
-		int bytesRead;
 		char *message = NULL;
 		char *fullMessage = NULL;
 		char buffer[4097];
+		int clientList[FD_SETSIZE];
+		int currentClients = 0;
 		memset(buffer, 0, 4097);
+		socklen_t len;
 		struct sockaddr_in addr;
 		int port = atoi(argv[1]);
 		if (port <= 0)
@@ -51,13 +54,20 @@ int main(int argc, char *argv[]) {
 		addr.sin_port = htons(port);
 		int clientId = 0;
 		client clients[FD_SETSIZE];
+		memset(clients, 0, sizeof(clients));
 		server = socket(AF_INET, SOCK_STREAM, 0);
-		if (server == -1)
+		if (server == -1) {
+			dprintf(2, "Call to socket failed\n");
 			fatalError();
-		if (bind(server, (struct sockaddr *) &addr, (socklen_t) sizeof(addr)))
+		}
+		if (bind(server, (struct sockaddr *) &addr, (socklen_t) sizeof(addr))) {
+			dprintf(2, "Call to bind failed\n");
 			fatalError();
-		if (listen(server, 4096))
+		}
+		if (listen(server, 4096)) {
+			dprintf(2, "Call to listen failed\n");
 			fatalError();
+		}
 		fd_set currentSocket, readySocket;
 		FD_ZERO(&currentSocket);
 		FD_SET(server, &currentSocket);
@@ -66,23 +76,24 @@ int main(int argc, char *argv[]) {
 			if (select(FD_SETSIZE, &readySocket, NULL, NULL, NULL) < 0) {
 				fatalError();
 			}
-			for (int i = 0; i < FD_SETSIZE; ++i) {
-				if (FD_ISSET(i, &readySocket)) {
-					if (i == server) {
-						printf("new client %d is connected", clientId);
-						clients[i].clientFd = accept(server, (struct sockaddr *) &addr, sizeof(addr));
-						sprintf(buffer, "server: client %d just arrived\n", clientId);
-						clients[i].clientId = clientId++;
-						FD_SET(clients[i].clientFd, &currentSocket);
-						for (int i = 0; i < FD_SETSIZE; i++) {
-							send(clients[i].clientFd, buffer, strlen(buffer), 0);
-						}
-						memset(buffer, 0, 4097);
-					}
-					else {
-						// message = getMessage(i);
-						// snprintf(buffer, 4096, "Client %d: ", clients[i])
-						// fullMessage = realloc()
+			if (FD_ISSET(server, &readySocket)) { //handle client connection
+				int client = accept(server, (struct sockaddr *) &addr, &len);
+				clients[client].clientFd = client;
+				clients[client].clientId = clientId;
+				clientList[clientId] = client;
+				FD_SET(client, &currentSocket);
+				sprintf(buffer, "Client %d just arrived\n", clientId);
+				currentClients++;
+				clientId++; 
+				for (int i = 0; i < currentClients; i++) {
+					send(clientList[i], buffer, strlen(buffer), 0);
+				}
+				memset(buffer, 0, 4097);
+			}
+			else { //handle possible client messages
+				for (int i = 0; i < currentClients; i++) {
+					if (FD_ISSET(clientList[i], &readySocket)) {
+						message = getMessage(clientList[i]);
 					}
 				}
 			}
