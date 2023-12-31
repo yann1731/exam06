cd138aa7de158d065f253a8ff6f15928df719e62
yann <yan_1731@hotmail.com>
2023-12-31 17:15:40 -0500

    Seems to be working

--- mini_serv.c
+++ mini_serv.c
@@ -17,38 +17,19 @@ typedef struct s_client {
 	int clientFd;
 } client;
 
-char *getMessage(int fd) {
-	char *message = NULL;
-	char buffer[4097];
-	memset(buffer, 0, 4097);
-	int bytesRead = 0;
-	do {
-		message = realloc(message, bytesRead + 1);
-		if (!message)
-			fatalError();
-		message = strcat(message, buffer);
-		message[bytesRead] = 0;
-	}
-	while (bytesRead = recv(fd, buffer, 4096, 0));
-	
-	return message;
-}
-
 int main(int argc, char *argv[]) {
 	if (argc == 2) {
 		printf("Server starting\n");
 		int server;
-		char *message = NULL;
-		char *fullMessage = NULL;
-		char buffer[4097];
+		char bufferOne[4097];
+		memset(bufferOne, 0, 4097);
+		char bufferTwo[4097];
+		memset(bufferTwo, 0, 4097);
 		int clientList[FD_SETSIZE];
 		int currentClients = 0;
-		memset(buffer, 0, 4097);
-		socklen_t len;
 		struct sockaddr_in addr;
+		socklen_t len;
 		int port = atoi(argv[1]);
-		if (port <= 0)
-			fatalError();
 		addr.sin_family = AF_INET;
 		addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 		addr.sin_port = htons(port);
@@ -56,24 +37,19 @@ int main(int argc, char *argv[]) {
 		client clients[FD_SETSIZE];
 		memset(clients, 0, sizeof(clients));
 		server = socket(AF_INET, SOCK_STREAM, 0);
-		if (server == -1) {
-			dprintf(2, "Call to socket failed\n");
+		if (server == -1)
 			fatalError();
-		}
-		if (bind(server, (struct sockaddr *) &addr, (socklen_t) sizeof(addr))) {
-			dprintf(2, "Call to bind failed\n");
+		if (bind(server, (struct sockaddr *) &addr, (socklen_t) sizeof(addr)))
 			fatalError();
-		}
-		if (listen(server, 4096)) {
-			dprintf(2, "Call to listen failed\n");
+		if (listen(server, 4096))
 			fatalError();
-		}
 		fd_set currentSocket, readySocket;
 		FD_ZERO(&currentSocket);
 		FD_SET(server, &currentSocket);
 		while (1) {
 			readySocket = currentSocket;
-			if (select(FD_SETSIZE, &readySocket, NULL, NULL, NULL) < 0) {
+			int valueFromSelect = 0;
+			if ((valueFromSelect = select(FD_SETSIZE, &readySocket, NULL, NULL, NULL)) < 0) {
 				fatalError();
 			}
 			if (FD_ISSET(server, &readySocket)) { //handle client connection
@@ -82,18 +58,42 @@ int main(int argc, char *argv[]) {
 				clients[client].clientId = clientId;
 				clientList[clientId] = client;
 				FD_SET(client, &currentSocket);
-				sprintf(buffer, "Client %d just arrived\n", clientId);
+				sprintf(bufferOne, "Client %d just arrived\n", clientId);
 				currentClients++;
 				clientId++; 
 				for (int i = 0; i < currentClients; i++) {
-					send(clientList[i], buffer, strlen(buffer), 0);
+					send(clientList[i], bufferOne, strlen(bufferOne), 0);
 				}
-				memset(buffer, 0, 4097);
+				memset(bufferOne, 0, 4097);
 			}
 			else { //handle possible client messages
 				for (int i = 0; i < currentClients; i++) {
 					if (FD_ISSET(clientList[i], &readySocket)) {
-						message = getMessage(clientList[i]);
+						int bytesRead = recv(clientList[i], bufferTwo, 4096, 0);
+						if (bytesRead > 0) { //handle message
+							sprintf(bufferOne, "Client %d: ", clients[clientList[i]].clientId);
+							strcat(bufferOne, bufferTwo);
+							for (int j = 0; j < currentClients; j++) {
+								if (j != i) {
+									send(clientList[j], bufferOne, strlen(bufferOne), 0);
+								}
+							}
+						}
+						else { //handle disconnect
+							sprintf(bufferOne, "server: client %d just left\n", clients[clientList[i]].clientId);
+							FD_CLR(clientList[i], &currentSocket);
+							clients[clientList[i]].clientId = 0;
+							clients[clientList[i]].clientFd = 0;
+							close(clientList[i]);
+							clientList[i] = 0;
+							for (int j = 0; j < currentClients; j++) {
+								if (j != i) {
+									send(clientList[j], bufferOne, strlen(bufferOne), 0);
+								}
+							}
+						}
+						memset(bufferOne, 0, 4097);
+						memset(bufferTwo, 0, 4097);
 					}
 				}
 			}
